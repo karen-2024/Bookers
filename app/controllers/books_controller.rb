89d8c7d1385a3a29002e48d8bf565bf5aa3060e2
EC2 Default User <177392295+karen-2024@users.noreply.github.com>
2024-09-23 12:33:15 +0000

    [Add] 保存機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,11 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
 
   def index
   end
@@ -13,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  #ストロングパラメータ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
