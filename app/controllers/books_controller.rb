caaa7f36f44fb7aadf743da4e916c562cef84b8e
EC2 Default User <177392295+karen-2024@users.noreply.github.com>
2024-09-25 14:43:29 +0000

    [Add] indexページ作成2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
@@ -12,21 +8,27 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new(book_params)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   #ストロングパラメータ
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 
 end
